d762c18e168af73e8a8f2954d3205696969e087b
Alexa EsLo <t23alexa-karime@utbispuebla.edu.mx>
2026-04-14 11:41:22 -0600

    Cambios en General.css

--- resources/views/components/tip-card.blade.php
+++ resources/views/components/tip-card.blade.php
@@ -12,3 +12,26 @@
         See Tip
     </a>
 </div>
+
+<div class="tip-card">
+
+    @if($image)
+        <img src="{{ asset('storage/' . $image) }}" class="tip-image">
+    @endif
+
+    <h3 class="tip-title">{{ $title }}</h3>
+
+    <p class="tip-description">
+        {{ Str::limit($description, 100) }}
+    </p>
+
+    <div class="tip-footer">
+        <span class="tip-likes">❤️ {{ $likes }}</span>
+        <span class="tip-author">{{ $author }}</span>
+    </div>
+
+    <a href="{{ route('tips.show', $id) }}" class="btn-tip">
+        See Tip
+    </a>
+
+</div>
